5ff3234c9bfff5f3896a6059d0058ca78d889ea2
Logan Watkins <lw2014wa@gmail.com>
2020-08-05 08:35:09 -0500

    InitCommit
    Initial Commit

---
+++ AlgorithmsUCSD/src/UCSDAlgorithmAssignments/Assignment1UCSD.java
@@ -0,0 +1,17 @@
+package UCSDAlgorithmAssignments;
+import java.util.Scanner;
+
+public class Assignment1UCSD {
+	
+	 static int sumOfTwoDigits(int first_digit, int second_digit) {
+	        return first_digit + second_digit;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        int a = s.nextInt();
+	        int b = s.nextInt();
+	        System.out.println(sumOfTwoDigits(a, b));
+	    }
+
+}
